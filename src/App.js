7eba4269fbb96e97b486e87c3d33856b0a764772
Ethanx0813 <ankityadav770587@gmail.com>
2024-01-07 20:06:04 +0530

    useeffect and usestate

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import './App.css';
 import Greeting from './Greeting';
 
 import Footer from './Footer';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 100000);
+
+    // Clean up the timer when the component is unmounted
+    return () => clearInterval(timer);
+  }, []); 
   return (
     <div>
-      <h1>Hello, React!</h1>
-      <Greeting name="John" message="Let's learn React together!" />
-      <Footer />
+      <h1>{count}</h1>
     </div>
   );
 }
+
 
 export default App;
