6a3773889b7956b1867318dae041614a5be27f8f
Ethanx0813 <ankityadav770587@gmail.com>
2024-01-08 13:02:57 +0530

    event hadnling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import Greeting from './Greeting';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import Footer from './Footer';
-import React, { useState, useEffect } from 'react';
-
+import EventHandler from './EventHandler';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setCount((prevCount) => prevCount + 1);
-    }, 100000);
-
-    // Clean up the timer when the component is unmounted
-    return () => clearInterval(timer);
-  }, []); 
   return (
     <div>
-      <h1>{count}</h1>
+      <h1>Your App Component</h1>
+      <p>This is some content in your app.</p>
+      {/* Render the EventHandler component */}
+      <EventHandler />
     </div>
   );
 }
-
 
 export default App;
